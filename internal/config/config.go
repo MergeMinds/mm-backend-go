fd78e8fd6099e92849cfc09defd59febf30c29dc
Ксения Иванова <natimal.well@gmail.com>
2025-03-29 16:16:19 +0400

    refactor: change env config

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,43 +4,35 @@ import (
 	"fmt"
 	"os"
 	"strconv"
-	"strings"
 
-	"github.com/joho/godotenv"
+	"github.com/kelseyhightower/envconfig"
 )
 
 type Config struct {
-	LogLevel            string
-	PostgresUrl         string
-	RedisUrl            string
-	SessionCookieSecure bool
-	SessionCookieDomain string
-	AllowOrigins        []string
-	AdminUsername       string
-	AdminPassword       string
-	AdminEmail          string
+	LogLevel            string   `envconfig:"LOG_LEVEL" default:"debug"`
+	PostgresUrl         string   `envconfig:"POSTGRES_URL" default:"postgres://t3m8ch@localhost/productsdb"`
+	RedisUrl            string   `envconfig:"REDIS_URL" default:"redis://dragonfly:6379/0"`
+	SessionCookieSecure bool     `envconfig:"SESSION_COOKIE_SECURE" default:"false"`
+	SessionCookieDomain string   `envconfig:"SESSION_COOKIE_DOMAIN" default:"localhost:5173"`
+	AllowOrigins        []string `envconfig:"ALLOW_ORIGINS" default:"http://localhost:5173"`
+	AdminUsername       string   `envconfig:"ADMIN_USERNAME" default:"admin"`
+	AdminPassword       string   `envconfig:"ADMIN_PASSWORD" default:"123456"`
+	AdminEmail          string   `envconfig:"ADMIN_EMAIL" default:"admin@admin.ru"`
 }
 
 func LoadFromEnv() (*Config, error) {
-	err := godotenv.Load()
+	var config_env Config
+	err := envconfig.Process("", &config_env)
+
 	if err != nil {
-		fmt.Println(".env file not found")
+		fmt.Println("Failed to process env vars: %v", err)
 	}
 
 	sessionCookieSecure, err := strconv.ParseBool(os.Getenv("SESSION_COOKIE_SECURE"))
+	config_env.SessionCookieSecure = sessionCookieSecure
 	if err != nil {
 		return nil, err
 	}
 
-	return &Config{
-		LogLevel:            os.Getenv("LOG_LEVEL"),
-		PostgresUrl:         os.Getenv("POSTGRES_URL"),
-		RedisUrl:            os.Getenv("REDIS_URL"),
-		SessionCookieSecure: sessionCookieSecure,
-		SessionCookieDomain: os.Getenv("SESSION_COOKIE_DOMAIN"),
-		AllowOrigins:        strings.Split(os.Getenv("ALLOW_ORIGINS"), ","),
-		AdminUsername:       os.Getenv("ADMIN_USERNAME"),
-		AdminPassword:       os.Getenv("ADMIN_PASSWORD"),
-		AdminEmail:          os.Getenv("ADMIN_EMAIL"),
-	}, nil
+	return &config_env, nil
 }
