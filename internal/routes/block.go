c0fafc74ad4791a05b97e4715306db9e8a8238e6
ProstoLive <artem009saratov@gmail.com>
2025-04-07 16:39:54 +0400

    feat: Added structs for mapping block data

--- internal/routes/block.go
+++ internal/routes/block.go
@@ -1,6 +1,26 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+)
+
+type dataType struct {
+	Format string `json:"format" binding:"required"`
+	Text   string `json:"text" binding:"required"`
+}
+
+type blockType struct {
+	Id        uuid.UUID `json:"id" binding:"required"`
+	BlockType string    `json:"blockType" binding:"required"`
+	Data      dataType  `json:"data" binding:"required"`
+	CourseId  uuid.UUID `json:"courseId" binding:"required"`
+}
+
+type createBlockType struct {
+	BlockType string
+	Data      dataType
+}
 
 func GetBlock(c *gin.Context, blockId string) {}
 
