8fbbc31304e07e526bc25a16c796f758c5317ee9
ProstoLive <artem009saratov@gmail.com>
2025-04-08 15:57:50 +0400

    docs: Documented block routes

--- go.mod
+++ go.mod
@@ -9,22 +9,22 @@ require (
 	github.com/google/uuid v1.6.0
 	github.com/joho/godotenv v1.5.1
 	github.com/redis/go-redis/v9 v9.7.1
-	github.com/swaggo/swag v1.8.12
+	github.com/swaggo/swag v1.16.4
 	go.uber.org/zap v1.27.0
-	golang.org/x/crypto v0.34.0
+	golang.org/x/crypto v0.37.0
 )
 
 require (
 	github.com/KyleBanks/depth v1.2.1 // indirect
-	github.com/PuerkitoBio/purell v1.1.1 // indirect
+	github.com/PuerkitoBio/purell v1.2.1 // indirect
 	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
-	github.com/go-openapi/jsonpointer v0.19.5 // indirect
-	github.com/go-openapi/jsonreference v0.19.6 // indirect
-	github.com/go-openapi/spec v0.20.4 // indirect
-	github.com/go-openapi/swag v0.19.15 // indirect
+	github.com/go-openapi/jsonpointer v0.21.1 // indirect
+	github.com/go-openapi/jsonreference v0.21.0 // indirect
+	github.com/go-openapi/spec v0.21.0 // indirect
+	github.com/go-openapi/swag v0.23.1 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
-	github.com/mailru/easyjson v0.7.6 // indirect
-	golang.org/x/tools v0.21.1-0.20240508182429-e35e4ccd0d2d // indirect
+	github.com/mailru/easyjson v0.9.0 // indirect
+	golang.org/x/tools v0.32.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
 
@@ -55,9 +55,9 @@ require (
 	github.com/ugorji/go/codec v1.2.12 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/arch v0.14.0 // indirect
-	golang.org/x/net v0.35.0 // indirect
-	golang.org/x/sys v0.30.0 // indirect
-	golang.org/x/text v0.22.0 // indirect
+	golang.org/x/net v0.39.0 // indirect
+	golang.org/x/sys v0.32.0 // indirect
+	golang.org/x/text v0.24.0 // indirect
 	google.golang.org/protobuf v1.36.5 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/routes/block.go
+++ internal/routes/block.go
@@ -5,27 +5,77 @@ import (
 	"github.com/google/uuid"
 )
 
-type dataType struct {
-	Format string `json:"format" binding:"required"`
-	Text   string `json:"text" binding:"required"`
+type DataType struct {
+	Format string `json:"format" `
+	Text   string `json:"text"`
 }
 
-type blockType struct {
+type BlockModel struct {
 	Id        uuid.UUID `json:"id" binding:"required"`
 	BlockType string    `json:"blockType" binding:"required"`
-	Data      dataType  `json:"data" binding:"required"`
+	Data      DataType  `json:"data" binding:"required"`
 	CourseId  uuid.UUID `json:"courseId" binding:"required"`
 }
 
-type createBlockType struct {
-	BlockType string
-	Data      dataType
+type BlockModelResponse struct {
+	BlockType string    `json:"blockType" binding:"required"`
+	Data      DataType  `json:"data" binding:"required"`
+	CourseId  uuid.UUID `json:"courseId" binding:"required"`
 }
 
+type CreateBlockType struct {
+	BlockType string   `json:"blockType" binding:"required"`
+	Data      DataType `json:"data" binding:"required"`
+}
+
+// @description Get block data
+// @summary Get block data
+// @tags blocks
+// @produce json
+// @param blockId path int true "Block ID"
+// @success 201 {object} BlockModelResponse
+// @failure 400 {object} apierr.ApiError "Invalid ID"
+// @failure 404 {object} apierr.ApiError "Block not found"
+// @failure 500 {object} apierr.ApiError "Internal server error"
+// @router /block [GET]
 func GetBlock(c *gin.Context, blockId string) {}
 
+// @description Register a new account
+// @summary Register a new account
+// @tags blocks
+// @accept json
+// @produce json
+// @param request body CreateBlockType true "Block payload"
+// @success 201 {object} BlockModelResponse
+// @failure 400 {object} apierr.ApiError "Invalid JSON"
+// @failure 403 {object} apierr.ApiError "No permission"
+// @failure 500 {object} apierr.ApiError "Internal server error"
+// @router /block [POST]
 func CreateBlock(c *gin.Context) {}
 
+// @description Change single or multiple parameters of the block
+// @summary Modify block
+// @tags blocks
+// @accept json
+// @produce json
+// @param blockId path int true "Block ID"
+// @param request body CreateBlockType true "Block payload"
+// @success 200 {object} BlockModelResponse
+// @failure 400 {object} apierr.ApiError "Invalid ID"
+// @failure 404 {object} apierr.ApiError "Parameter not found"
+// @failure 404 {object} apierr.ApiError "Block not found"
+// @failure 500 {object} apierr.ApiError "Internal server error"
+// @router /block [PATCH]
 func PatchBlock(c *gin.Context, blockId string) {}
 
+// @description Will remove block from course but won't delete it from database
+// @summary Remove block
+// @tags blocks
+// @produce json
+// @param blockId path int true "Block ID"
+// @success 204
+// @failure 400 {object} apierr.ApiError "Invalid ID"
+// @failure 404 {object} apierr.ApiError "Block not found"
+// @failure 500 {object} apierr.ApiError "Internal server error"
+// @router /block [DELETE]
 func DeleteBlock(c *gin.Context, blockId string) {}
